22f3221eb35d198dc4d47b338cc0a1282e0d0a02
Guillermo Rodriguez <48388462+guille-developer@users.noreply.github.com>
2022-05-05 21:55:13 -0300

    se agrega controlador de api usuario

---
+++ src/main/java/com/log/app/controllers/UsuarioRestController.java
@@ -0,0 +1,27 @@
+package com.log.app.controllers;
+
+import com.log.app.entidades.Usuario;
+import com.log.app.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UsuarioRestController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public Usuario registrarse(@RequestBody Usuario usuario) {
+        return userService.createUser(usuario);
+    }
+
+    @PostMapping("/autenticate")
+    public Usuario logearse(String email, String password) {
+        return userService.authenticateUsuario(email, password);
+    }
+}
