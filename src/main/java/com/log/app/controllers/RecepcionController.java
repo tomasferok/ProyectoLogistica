44e7ac1fdca7c943ee3d819af0c1e30ad6ba486a
Guillermo Rodriguez <48388462+guille-developer@users.noreply.github.com>
2022-05-05 21:47:42 -0300

    se crea base de pagina crear recepcion, aun no esta conectada al backend

--- src/main/java/com/log/app/controllers/RecepcionController.java
+++ src/main/java/com/log/app/controllers/RecepcionController.java
@@ -1,13 +1,22 @@
 package com.log.app.controllers;
 
+import com.log.app.entidades.Recepcion;
 import com.log.app.services.RecepcionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RecepcionController {
     
     @Autowired
     private RecepcionService recepcionService;
+
+    @GetMapping(value = "/crearRecepcion")
+    public String login(Model model) {
+        model.addAttribute("recepcion", new Recepcion());
+        return "crearRecepcion";
+    }
 }
