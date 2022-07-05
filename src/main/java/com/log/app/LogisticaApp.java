e1bd14d952adf5799f83eb70e303b34abb21f540
Guillermo Rodriguez <48388462+guille-developer@users.noreply.github.com>
2022-07-05 18:30:21 -0300

    se corriguen errores

--- src/main/java/com/log/app/LogisticaApp.java
+++ src/main/java/com/log/app/LogisticaApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-/**
+/** 
  * Clase Main
  * 
  * @author ClawTech - UTEC
